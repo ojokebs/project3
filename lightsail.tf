435447dc6ccf99528914b1d4d5acab748fb907ca
ojokebs <ojokebs@gmail.com>
2023-09-20 21:05:42 -0400

    project3 push

---
+++ lightsail.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  alias  = "lightsail"
+  region = "us-east-1"
+}
+
+resource "aws_lightsail_instance" "web_server" {
+  name              = "john"
+  availability_zone = "us-east-1a"
+  blueprint_id      = "centos_7_1901_01"
+  bundle_id         = "nano_2_0"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install unzip wget httpd -y
+    sudo wget https://github.com/utrains/static-resume/archive/refs/heads/main.zip
+    sudo unzip main.zip
+    sudo rm -rf /var/www/html/*
+    sudo cp -r static-resume-main/* /var/www/html/
+    sudo systemctl start httpd
+    sudo systemctl enable httpd
+    EOF
+}
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "public_ip" {
+    value = "aws_lightsail_instance.web_server.public_ip_address"
+  
+}
